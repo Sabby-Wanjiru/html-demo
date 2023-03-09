c1c01f2c092c9781564e79090e98f339da496f2a
Sabby-Wanjiru <124573514+Sabby-Wanjiru@users.noreply.github.com>
2023-03-09 13:30:26 +0300

    Add files via upload
    Dart Control Flow assignment for Sabina Wanjiru

---
+++ controlflowstatement.dart
@@ -0,0 +1,11 @@
+void main() {
+  var num = 5;
+  if (num > 10) {
+    print("Your is greater than 10");
+  } else if (num < 10) {
+    print("Your number is less than 10");
+  } else 
+    if(num==10) {
+    print("your number is equal to 10");
+  }
+}
